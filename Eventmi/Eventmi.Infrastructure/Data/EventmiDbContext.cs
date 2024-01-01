483698ec7cdaaac2aa29b2e3ec65b5d37e8545a0
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-01 19:33:46 +0200

    Added DB config

--- Eventmi/Eventmi.Infrastructure/Data/EventmiDbContext.cs
+++ Eventmi/Eventmi.Infrastructure/Data/EventmiDbContext.cs
@@ -1,17 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Eventmi.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Eventmi.Infrastructure.Data
 {
+    /// <summary>
+    /// Db context
+    /// </summary>
     public class EventmiDbContext : DbContext
     {
+        /// <summary>
+        /// Creates db context without settings
+        /// </summary>
         public EventmiDbContext()
         {
 
         }
 
+        /// <summary>
+        /// Creates db context with options
+        /// </summary>
+        /// <param name="options">Db context options</param>
         public EventmiDbContext(DbContextOptions<EventmiDbContext> options) : base(options)
         {
 
         }
+
+        /// <summary>
+        /// Events table
+        /// </summary>
+        public DbSet<Event> Events { get; set; } = null!;
     }
 }
--- Eventmi/Eventmi/Program.cs
+++ Eventmi/Eventmi/Program.cs
@@ -1,7 +1,13 @@
+using Eventmi.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<EventmiDbContext>(options=>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EventmiConnection")));
 
 var app = builder.Build();
 
